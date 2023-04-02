5614af19348af016971354020ac239a72912c3d1
José Gonçalves <joseinacio774@gmail.com>
2023-04-02 19:03:45 -0300

    created listviwer evolution

--- lib/core/widgets/evolution_pokemon_widget.dart
+++ lib/core/widgets/evolution_pokemon_widget.dart
@@ -18,46 +18,53 @@ class EvolutionPokemonWidget extends StatefulWidget {
 }
 
 class _EvolutionPokemonWidgetState extends State<EvolutionPokemonWidget> {
-  List<dynamic> evolutions = [];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.3,
+      height: MediaQuery.of(context).size.height * 0.2,
       child: Column(
         children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Evoluções'.toUpperCase(),
-                  style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.05,
-                      fontWeight: FontWeight.w900,
-                      color:
-                          Backgroud().getBackgroudType(type: widget.types[0]),
-                      fontFamily: 'Nunito'))
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Image.network(
-                    'http://www.serebii.net/pokemongo/pokemon/${widget.evolutions[0].num}.png',
-                    scale: 1.5,
+          Text('Evoluções'.toUpperCase(),
+              style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                  fontWeight: FontWeight.w900,
+                  color: Backgroud().getBackgroudType(type: widget.types[0]),
+                  fontFamily: 'Nunito')),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.evolutions.length,
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        child: Column(
+                          children: [
+                            Image.network(
+                              'http://www.serebii.net/pokemongo/pokemon/${widget.evolutions[index].num}.png',
+                              scale: 1.6,
+                            ),
+                            Text(widget.evolutions[index].name,
+                                style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.width *
+                                            0.03,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.grey.withOpacity(0.8),
+                                    fontFamily: 'Nunito')),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(widget.evolutions[0].name,
-                      style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width * 0.03,
-                          fontWeight: FontWeight.w900,
-                          color: Colors.grey.withOpacity(0.8),
-                          fontFamily: 'Nunito')),
-                ],
-              ),
-            ],
+                );
+              },
+            ),
           )
         ],
       ),
